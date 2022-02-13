bc5617326022986d205770e0182d07427efd3fa4
Akshay1997a <akshaydighorikar@gmail.com>
2022-02-13 18:53:27 +0530

    Added splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,17 @@
  * @format
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {ThemeProvider} from 'styled-components';
 import RootNavigator from './navigations/RootNavigator';
 import {darkTheme, lightTheme} from './theme/themes';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <ThemeProvider theme={lightTheme}>
       <RootNavigator />
