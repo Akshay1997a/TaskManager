d324d19a84a0e0734586a727f358eccb0a56494f
Akshay1997a <akshaydighorikar@gmail.com>
2022-03-15 20:42:34 +0530

    updated login functon with proper error alerts

--- src/store/auth/authThunks.ts
+++ src/store/auth/authThunks.ts
@@ -2,7 +2,9 @@ import {createAsyncThunk} from '@reduxjs/toolkit';
 import auth, {firebase} from '@react-native-firebase/auth';
 import {setLoading} from '../loading/loadingSlice';
 import {updateUser} from '../user/userSlice';
-import {Alert} from 'react-native';
+import {Alert} from '../../components/Alert/AlertProvider';
+import {AUTH_ERRORS} from '../../config/FirebaseErrorCodes';
+import MESSAGES from '../../config/messages';
 
 interface AuthInterface {
   email: string;
@@ -15,12 +17,30 @@ export const login = createAsyncThunk(
     dispatch(setLoading(true));
     try {
       let res = await auth().signInWithEmailAndPassword(email, password);
-      console.log(res);
       dispatch(updateUser(res.user.toJSON() as any));
       return await res.user.getIdToken();
-    } catch (error) {
+    } catch (error: any) {
       console.error(error);
-      return error;
+      switch (error.code) {
+        case AUTH_ERRORS.INVALID_EMAIL:
+          Alert.show(MESSAGES.INVALID_EMAIL_ID);
+          break;
+
+        case AUTH_ERRORS.USER_NOT_FOUND:
+          Alert.show(MESSAGES.INVALID_EMAIL_ID);
+          break;
+
+        case AUTH_ERRORS.WRONG_PASSWORD:
+          Alert.show(MESSAGES.INVALID_EMAIL_ID);
+          break;
+
+        case AUTH_ERRORS.TOO_MANY_REQUESTS:
+          Alert.show(MESSAGES.REQUEST_BLOCK);
+          break;
+
+        default:
+          Alert.show(MESSAGES.ERROR);
+      }
     } finally {
       dispatch(setLoading(false));
     }
