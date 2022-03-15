dc01564c88c9c203c87c4e679b3a037d40b03119
Akshay1997a <akshaydighorikar@gmail.com>
2022-03-15 20:42:13 +0530

    Added error codes and messages

--- src/config/FirebaseErrorCodes.ts
+++ src/config/FirebaseErrorCodes.ts
@@ -1,3 +1,7 @@
 export const AUTH_ERRORS = {
+  INVALID_EMAIL: 'auth/invalid-email',
+  INACTIVE_USER: 'auth/user-disabled',
+  USER_NOT_FOUND: 'auth/user-not-found',
   WRONG_PASSWORD: 'auth/wrong-password',
+  TOO_MANY_REQUESTS: 'auth/too-many-requests',
 };
---
+++ src/config/messages.ts
@@ -0,0 +1,7 @@
+const MESSAGES = {
+  INVALID_EMAIL_ID: 'Email id is invalid. Please try again.',
+  REQUEST_BLOCK: 'Request has been blocked.',
+  ERROR: 'Opps something went wrong.',
+};
+
+export default MESSAGES;
